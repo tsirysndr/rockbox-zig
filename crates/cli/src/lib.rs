0ee7b42d40830cf26b97ab3303493b3239823a87
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-09-18 19:25:26 +0000

    cli: fix pointer issue

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -2,13 +2,28 @@ use clap::Command;
 use owo_colors::OwoColorize;
 use std::ffi::CStr;
 
+#[macro_export]
+macro_rules! cast_ptr {
+    ($ptr:expr) => {{
+        #[cfg(target_arch = "aarch64")]
+        {
+            $ptr as *const u8
+        }
+
+        #[cfg(not(target_arch = "aarch64"))]
+        {
+            $ptr as *const i8
+        }
+    }};
+}
+
 #[no_mangle]
 pub extern "C" fn parse_args(argc: usize, argv: *const *const u8) -> i32 {
     let string_array = unsafe { std::slice::from_raw_parts(argv, argc) };
     let args: Vec<&str> = string_array
         .iter()
         .map(|&ptr| {
-            let c_str = unsafe { CStr::from_ptr(ptr as *const i8) };
+            let c_str = unsafe { CStr::from_ptr(cast_ptr!(ptr)) };
             c_str
                 .to_str()
                 .unwrap_or("[Invalid UTF-8 or Non Null-Terminated String]")
