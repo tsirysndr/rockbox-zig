5a7dfdb98ddf857a877dcbcfea89ff39e8d229bd
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-10-03 09:41:12 +0000

    http-api: rewrite get_tree_entries function

--- crates/server/src/handlers/browse.rs
+++ crates/server/src/handlers/browse.rs
@@ -1,6 +1,13 @@
+use std::fs;
+
 use crate::http::{Context, Request, Response};
 use anyhow::Error;
-use rockbox_sys as rb;
+use rockbox_sys::types::tree::Entry;
+
+const AUDIO_EXTENSIONS: [&str; 17] = [
+    "mp3", "ogg", "flac", "m4a", "aac", "mp4", "alac", "wav", "wv", "mpc", "aiff", "ac3", "opus",
+    "spx", "sid", "ape", "wma",
+];
 
 pub async fn get_tree_entries(
     _ctx: &Context,
@@ -11,21 +18,42 @@ pub async fn get_tree_entries(
         Some(path) => path.as_str().unwrap_or("/"),
         None => "/",
     };
-    if let Err(e) = rb::browse::rockbox_browse_at(path) {
-        if e.to_string().starts_with("No such file or directory") {
-            res.set_status(404);
-            return Ok(());
-        }
+    let show_hidden = req.query_params.get("show_hidden").is_some();
+
+    if !fs::metadata(path)?.is_dir() {
         res.set_status(500);
+        res.text("Path is not a directory");
         return Ok(());
     }
 
     let mut entries = vec![];
-    let context = rb::browse::tree_get_context();
 
-    for i in 0..context.filesindir {
-        let entry = rb::browse::tree_get_entry_at(i);
-        entries.push(entry);
+    for file in fs::read_dir(path)? {
+        let file = file?;
+
+        if file.metadata()?.is_file()
+            && !AUDIO_EXTENSIONS.iter().any(|ext| {
+                file.path()
+                    .to_string_lossy()
+                    .ends_with(&format!(".{}", ext))
+            })
+        {
+            continue;
+        }
+
+        if file.file_name().to_string_lossy().starts_with(".") && !show_hidden {
+            continue;
+        }
+
+        entries.push(Entry {
+            name: file.path().to_string_lossy().to_string(),
+            time_write: file
+                .metadata()?
+                .modified()?
+                .duration_since(std::time::SystemTime::UNIX_EPOCH)?
+                .as_secs() as u32,
+            ..Default::default()
+        });
     }
 
     res.json(&entries);
