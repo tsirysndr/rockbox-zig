964a3a2040cd9be70f50fc38ca27f7346da0c09b
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-10-04 15:40:47 +0000

    webui: fix storybook

---
+++ webui/rockbox/src/Components/Tracks/Tracks.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Tracks from "./Tracks";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Components/Tracks",
+  component: Tracks,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof Tracks>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default: Story = {
+  args: {},
+};
