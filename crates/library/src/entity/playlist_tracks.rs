ad4eace8ea8203187539e8cc1f8102a716ae3286
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-12-26 02:35:08 +0100

    playlist: add db queries for playlist

--- crates/library/src/entity/playlist_tracks.rs
+++ crates/library/src/entity/playlist_tracks.rs
@@ -1,6 +1,10 @@
+use chrono::{DateTime, Utc};
+
 #[derive(sqlx::FromRow, Default)]
 pub struct PlaylistTracks {
     pub id: String,
     pub playlist_id: String,
     pub track_id: String,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub created_at: DateTime<Utc>,
 }
