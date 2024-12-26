4eb52879a613be7d9b99e888679cd1c296e5b06d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-12-26 02:35:08 +0100

    playlist: add db queries for playlist

--- crates/library/src/repo/folder.rs
+++ crates/library/src/repo/folder.rs
@@ -1,8 +1,73 @@
 use crate::entity::folder::Folder;
-use sqlx::{Pool, Sqlite};
+use sqlx::{types::chrono, Pool, Sqlite};
 
-pub async fn save(pool: Pool<Sqlite>, folder: Folder) {}
+pub async fn save(pool: Pool<Sqlite>, folder: Folder) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+        INSERT INTO folder (
+            id,
+            name,
+            created_at,
+            updated_at
+        )
+        VALUES ($1, $2, $3, $4)
+    "#,
+    )
+    .bind(folder.id)
+    .bind(&folder.name)
+    .bind(folder.created_at)
+    .bind(folder.updated_at)
+    .execute(&pool)
+    .await?;
+    Ok(())
+}
 
-pub async fn find(pool: Pool<Sqlite>) {}
+pub async fn find(pool: Pool<Sqlite>, id: &str) -> Result<Option<Folder>, sqlx::Error> {
+    sqlx::query_as::<_, Folder>(r#"SELECT * FROM folder WHERE id = $1"#)
+        .bind(id)
+        .fetch_optional(&pool)
+        .await
+}
 
-pub async fn all(pool: Pool<Sqlite>) {}
+pub async fn find_by_parent(
+    pool: Pool<Sqlite>,
+    parent_id: &str,
+) -> Result<Vec<Folder>, sqlx::Error> {
+    sqlx::query_as::<_, Folder>(r#"SELECT * FROM folder WHERE parent_id = $1"#)
+        .bind(parent_id)
+        .fetch_all(&pool)
+        .await
+}
+
+pub async fn all(pool: Pool<Sqlite>) -> Result<Vec<Folder>, sqlx::Error> {
+    sqlx::query_as::<_, Folder>(r#"SELECT * FROM folder ORDER BY name ASC"#)
+        .fetch_all(&pool)
+        .await
+}
+
+pub async fn delete(pool: Pool<Sqlite>, id: &str) -> Result<(), sqlx::Error> {
+    sqlx::query(r#"DELETE FROM folder WHERE id = $1"#)
+        .bind(id)
+        .execute(&pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn update(pool: Pool<Sqlite>, folder: Folder) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+            UPDATE folder SET
+                name = $2,
+                updated_at = $3,
+                parent_id = $4
+            WHERE id = $1
+        "#,
+    )
+    .bind(&folder.id)
+    .bind(&folder.name)
+    .bind(chrono::Utc::now())
+    .bind(&folder.parent_id)
+    .execute(&pool)
+    .await?;
+    Ok(())
+}
--- crates/library/src/repo/playlist.rs
+++ crates/library/src/repo/playlist.rs
@@ -1,8 +1,79 @@
 use crate::entity::playlist::Playlist;
 use sqlx::{Pool, Sqlite};
 
-pub async fn save(pool: Pool<Sqlite>, playlist: Playlist) {}
+pub async fn save(pool: Pool<Sqlite>, playlist: Playlist) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+        INSERT INTO playlist (
+            id,
+            name,
+            folder_id,
+            created_at,
+            updated_at
+        )
+        VALUES ($1, $2, $3, $4, $5)
+    "#,
+    )
+    .bind(&playlist.id)
+    .bind(&playlist.name)
+    .bind(&playlist.folder_id)
+    .bind(playlist.created_at)
+    .bind(playlist.updated_at)
+    .execute(&pool)
+    .await?;
+    Ok(())
+}
 
-pub async fn find(pool: Pool<Sqlite>) {}
+pub async fn find(pool: Pool<Sqlite>, id: &str) -> Result<Option<Playlist>, sqlx::Error> {
+    sqlx::query_as::<_, Playlist>(r#"SELECT * FROM playlist WHERE id = $1"#)
+        .bind(id)
+        .fetch_optional(&pool)
+        .await
+}
 
-pub async fn all(pool: Pool<Sqlite>) {}
+pub async fn find_by_folder(
+    pool: Pool<Sqlite>,
+    folder_id: &str,
+) -> Result<Vec<Playlist>, sqlx::Error> {
+    sqlx::query_as::<_, Playlist>(
+        r#"
+            SELECT * FROM playlist WHERE folder_id = $1 ORDER BY name ASC   
+         "#,
+    )
+    .bind(folder_id)
+    .fetch_all(&pool)
+    .await
+}
+
+pub async fn all(pool: Pool<Sqlite>) -> Result<Vec<Playlist>, sqlx::Error> {
+    sqlx::query_as::<_, Playlist>(r#"SELECT * FROM playlist ORDER BY name ASC"#)
+        .fetch_all(&pool)
+        .await
+}
+
+pub async fn delete(pool: Pool<Sqlite>, id: &str) -> Result<(), sqlx::Error> {
+    sqlx::query(r#"DELETE FROM playlist WHERE id = $1"#)
+        .bind(id)
+        .execute(&pool)
+        .await?;
+    Ok(())
+}
+
+pub async fn update(pool: Pool<Sqlite>, playlist: Playlist) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+        UPDATE playlist SET
+            name = $2,
+            updated_at = $3,
+            folder_id = $4
+        WHERE id = $1
+    "#,
+    )
+    .bind(&playlist.id)
+    .bind(&playlist.name)
+    .bind(chrono::Utc::now())
+    .bind(&playlist.folder_id)
+    .execute(&pool)
+    .await?;
+    Ok(())
+}
