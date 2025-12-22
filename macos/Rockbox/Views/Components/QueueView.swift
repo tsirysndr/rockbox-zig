26018bf5df1a7bbe5db38e4540dd59abdfce09b4
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-12-22 10:01:51 +0300

    Adjust QueueView empty-state condition

--- macos/Rockbox/Views/Components/QueueView.swift
+++ macos/Rockbox/Views/Components/QueueView.swift
@@ -54,7 +54,7 @@ struct QueueView: View {
             }
 
             
-            if player.upNext.isEmpty && player.history.isEmpty {
+            if  (player.upNext.isEmpty && showPlayingNext) || player.history.isEmpty {
                 VStack(spacing: 12) {
                     Image(systemName: "music.note.list")
                         .font(.system(size: 32))
