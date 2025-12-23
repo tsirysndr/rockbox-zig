0873c6bb6781ba915d05c1717bc103ed7fb80b67
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-12-23 10:12:18 +0300

    Use env::var instead of env.var

--- cli/src/cmd/start.rs
+++ cli/src/cmd/start.rs
@@ -31,7 +31,7 @@ pub fn start(with_ui: bool) -> Result<(), Error> {
                     .env("ROCKBOX_PORT", port)
                     .env("ROCKBOX_GRAPHQL_PORT", ui_port)
                     .env("ROCKBOX_TCP_PORT", http_port)
-                    .env("ROCKBOX_UPDATE_LIBRARY", env.var("ROCKBOX_UPDATE_LIBRARY"))
+                    .env("ROCKBOX_UPDATE_LIBRARY", env::var("ROCKBOX_UPDATE_LIBRARY"))
                     .spawn()?;
 
                 child.wait()?;
