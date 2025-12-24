0c45936bd1022f013f57e1e29a45e8e090da3f1e
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-12-24 13:58:27 +0300

    Remove cast_ptr macro and cast argv to c_char

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -12,33 +12,13 @@ use std::time::Duration;
 use std::{env, ffi::CStr};
 use std::{fs, thread};
 
-#[macro_export]
-macro_rules! cast_ptr {
-    ($ptr:expr) => {{
-        #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
-        {
-            $ptr as *const i8
-        }
-
-        #[cfg(all(target_arch = "aarch64", target_os = "linux"))]
-        {
-            $ptr as *const u8
-        }
-
-        #[cfg(not(target_arch = "aarch64"))]
-        {
-            $ptr as *const i8
-        }
-    }};
-}
-
 #[no_mangle]
 pub extern "C" fn parse_args(argc: usize, argv: *const *const u8) -> i32 {
     let string_array = unsafe { std::slice::from_raw_parts(argv, argc) };
     let args: Vec<&str> = string_array
         .iter()
         .map(|&ptr| {
-            let c_str = unsafe { CStr::from_ptr(cast_ptr!(ptr)) };
+            let c_str = unsafe { CStr::from_ptr(ptr as *const std::ffi::c_char) };
             c_str
                 .to_str()
                 .unwrap_or("[Invalid UTF-8 or Non Null-Terminated String]")
